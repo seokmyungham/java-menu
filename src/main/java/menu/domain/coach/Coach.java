901ac8c9b5df640b7b5f8b1fed1e4d0479635af4
seokmyungham <hellotjraud@gmail.com>
2023-12-15 15:04:36 +0900

    feat(Coach): isCantEatMenu, alreadyRecommendedMenu 메서드 추가
    isCantEatMenu: 먹지 못하는 메뉴인지 확인합니다
    alreadyRecommendedMenu: 이미 추천했던 메뉴인지 확인합니다

--- src/main/java/menu/domain/coach/Coach.java
+++ src/main/java/menu/domain/coach/Coach.java
@@ -1,13 +1,24 @@
 package menu.domain.coach;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
     private Name name;
     private List<String> cantEatMenus;
+    private List<String> recommendedMenus;
 
     public Coach(Name name, List<String> cantEatMenus) {
         this.name = name;
         this.cantEatMenus = cantEatMenus;
+        this.recommendedMenus = new ArrayList<>();
+    }
+
+    public boolean isCantEatMenu(String menuName) {
+        return cantEatMenus.contains(menuName);
+    }
+
+    public boolean alreadyRecommendedMenu(String menuName) {
+        return recommendedMenus.contains(menuName);
     }
 }
