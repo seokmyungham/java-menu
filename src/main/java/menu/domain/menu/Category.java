f25e4ef75c82138597ba13153bd2f450e87a8e1c
seokmyungham <hellotjraud@gmail.com>
2023-12-15 14:49:12 +0900

    feat(Category): getCategoryByCode 메서드 추가
    getCategoryByCode: code 번호로 일치하는 카테고리를 반환하는 메서드입니다

--- src/main/java/menu/domain/menu/Category.java
+++ src/main/java/menu/domain/menu/Category.java
@@ -1,5 +1,7 @@
 package menu.domain.menu;
 
+import java.util.Arrays;
+
 public enum Category {
     JAPANESE("일식", 1),
     KOREAN("한식", 2),
@@ -7,11 +9,20 @@ public enum Category {
     ASIAN("아시안", 4),
     WESTERN("양식", 5);
 
+    public static final String INVALID_CODE_ERROR_MESSAGE = "[ERROR] 코드와 일치하는 카테고리를 찾지 못했습니다.";
+
     private String name;
     private int code;
 
     Category(String name, int code) {
         this.name = name;
         this.code = code;
+    }
+
+    public static Category getCategoryByCode(int code) {
+        return Arrays.stream(Category.values())
+                .filter(category -> category.code == code)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_CODE_ERROR_MESSAGE));
     }
 }
