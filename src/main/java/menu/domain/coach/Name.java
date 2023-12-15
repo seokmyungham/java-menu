00724585711be82485f8c2c6ae23ec23202e962a
seokmyungham <hellotjraud@gmail.com>
2023-12-15 14:03:38 +0900

    feat(Name): validateNameLength 메서드 추가

--- src/main/java/menu/domain/coach/Name.java
+++ src/main/java/menu/domain/coach/Name.java
@@ -1,9 +1,18 @@
 package menu.domain.coach;
 
+import static menu.constant.ErrorMessage.COACH_NAME_LENGTH_ERROR_MESSAGE;
+
 public class Name {
     private String name;
 
     public Name(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    public void validateNameLength(String input) {
+        if (input.length() < 2 || input.length() > 4) {
+            throw new IllegalArgumentException(COACH_NAME_LENGTH_ERROR_MESSAGE);
+        }
     }
 }
