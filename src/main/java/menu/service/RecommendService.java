bd51e876b846d91eabc13069f043e5ce2934939c
seokmyungham <hellotjraud@gmail.com>
2023-12-15 17:58:20 +0900

    refactor(RecommendService): 메서드 분리

--- src/main/java/menu/service/RecommendService.java
+++ src/main/java/menu/service/RecommendService.java
@@ -14,26 +14,37 @@ public class RecommendService {
 
     public Map<DayWeek, Category> service(Crew crew) {
         Map<DayWeek, Category> categoryHistory = new HashMap<>();
-
         for (DayWeek dayWeek : DayWeek.values()) {
-            Category category = Category.getCategoryByCode(Randoms.pickNumberInRange(1, 5));
-
-            while (Collections.frequency(categoryHistory.values(), category) > 2) {
-                category = Category.getCategoryByCode(Randoms.pickNumberInRange(1, 5));
-            }
-
+            Category category = recommendCategory(categoryHistory);
             categoryHistory.put(dayWeek, category);
-
-            for (Coach coach : crew.getCoaches()) {
-                String menu = Randoms.shuffle(Menu.getMenusByCategory(category)).get(0);
-
-                while (coach.alreadyRecommendedMenu(menu) || coach.isCantEatMenu(menu)) {
-                     menu = Randoms.shuffle(Menu.getMenusByCategory(category)).get(0);
-                }
-
-                coach.addRecommendMenu(menu);
-            }
+            recommendMenuToCoach(crew, category);
         }
         return categoryHistory;
+    }
+
+    private Category recommendCategory(Map<DayWeek, Category> categoryHistory) {
+        Category category = Category.getCategoryByCode(Randoms.pickNumberInRange(1, 5));
+        while (isCategoryOverMaximum(categoryHistory, category)) {
+            category = Category.getCategoryByCode(Randoms.pickNumberInRange(1, 5));
+        }
+        return category;
+    }
+
+    private boolean isCategoryOverMaximum(Map<DayWeek, Category> categoryHistory, Category category) {
+        return Collections.frequency(categoryHistory.values(), category) > 2;
+    }
+
+    private void recommendMenuToCoach(Crew crew, Category category) {
+        for (Coach coach : crew.getCoaches()) {
+            String menu = Randoms.shuffle(Menu.getMenusByCategory(category)).get(0);
+            while (cantRecommendMenu(coach, menu)) {
+                 menu = Randoms.shuffle(Menu.getMenusByCategory(category)).get(0);
+            }
+            coach.addRecommendMenu(menu);
+        }
+    }
+
+    private boolean cantRecommendMenu(Coach coach, String menu) {
+        return coach.alreadyRecommendedMenu(menu) || coach.isCantEatMenu(menu);
     }
 }
