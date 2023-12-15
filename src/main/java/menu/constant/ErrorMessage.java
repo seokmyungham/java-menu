8e2f870d727b8f354a91902ac6b541dffecafbac
seokmyungham <hellotjraud@gmail.com>
2023-12-15 15:20:30 +0900

    feat(ErrorMessage): 에러 메시지 추가

--- src/main/java/menu/constant/ErrorMessage.java
+++ src/main/java/menu/constant/ErrorMessage.java
@@ -4,5 +4,6 @@ public class ErrorMessage {
     public static final String COACH_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 코치 이름은 최소 2글자, 최대 4글자 사이여야 합니다.";
     public static final String INPUT_FORMAT_ERROR_MESSAGE = "[ERROR] 형식을 맞추어 다시 입력해주세요.";
     public static final String CREW_SIZE_ERROR_MESSAGE = "[ERROR] 함께 식사하는 코치는 최소 2명, 최대 5명까지 가능합니다.";
+    public static final String CANT_EAT_MENUS_SIZE_ERROR_MESSAGE = "[ERROR] 먹지 못하는 메뉴의 최대 개수는 2개입니다.";
 
 }
