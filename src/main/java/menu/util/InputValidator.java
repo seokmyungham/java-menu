a2b9baeade8a8e3a09a4855643f4fd3f7e281fab
seokmyungham <hellotjraud@gmail.com>
2023-12-15 17:51:35 +0900

    feat(InputValidator): validateCoachSize, validateCoachNameDuplicate 메서드 추가
    validateCoachSize: 입력으로 들어온 코치의 이름으로 코치 숫자를 검증합니다
    validateCoachNameDuplicate: 코치의 이름이 중복되었는지 검증합니다

--- src/main/java/menu/util/InputValidator.java
+++ src/main/java/menu/util/InputValidator.java
@@ -1,6 +1,12 @@
 package menu.util;
 
+import static menu.constant.ErrorMessage.COACH_NAME_DUPLICATED_ERROR_MESSAGE;
+import static menu.constant.ErrorMessage.CREW_SIZE_ERROR_MESSAGE;
 import static menu.constant.ErrorMessage.INPUT_FORMAT_ERROR_MESSAGE;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class InputValidator {
     private static final String COACH_NAME_INPUT_FORMAT = "^[가-힣a-zA-Z0-9]+(,[가-힣a-zA-Z0-9]+)*$";
@@ -8,6 +14,20 @@ public class InputValidator {
     public static void validateCoachNameInputFormat(String input) {
         if (!input.matches(COACH_NAME_INPUT_FORMAT)) {
             throw new IllegalArgumentException(INPUT_FORMAT_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateCoachSize(List<String> coachNames) {
+        if (coachNames.size() < 2 || coachNames.size() > 5) {
+            throw new IllegalArgumentException(CREW_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateCoachNameDuplicate(List<String> coachNames) {
+        Set<String> validate = new HashSet<>(coachNames);
+
+        if (coachNames.size() != validate.size()) {
+            throw new IllegalArgumentException(COACH_NAME_DUPLICATED_ERROR_MESSAGE);
         }
     }
 }
