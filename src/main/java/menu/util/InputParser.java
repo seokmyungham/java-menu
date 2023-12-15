57de5c13d112f949057a7030d3bdd95b8ffe305a
seokmyungham <hellotjraud@gmail.com>
2023-12-15 13:55:56 +0900

    feat(InputParser): 클래스 생성 및 parseInputByDelimiter 메서드 추가
    parseInputByDelimiter: 문자열을 파싱하는 메서드입니다

---
+++ src/main/java/menu/util/InputParser.java
@@ -0,0 +1,10 @@
+package menu.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputParser {
+    public static List<String> parseInputByDelimiter(String input, String DELIMITER) {
+        return new ArrayList<>(List.of(input.split(DELIMITER)));
+    }
+}
