027c2d556927d91b79dee2b19a63d980692bdcee
seokmyungham <hellotjraud@gmail.com>
2023-12-15 14:31:05 +0900

    feat(ErrorMessage): CREW_SIZE_ERROR_MESSAGE 에러 메시지 추가

--- src/main/java/menu/constant/ErrorMessage.java
+++ src/main/java/menu/constant/ErrorMessage.java
@@ -3,4 +3,6 @@ package menu.constant;
 public class ErrorMessage {
     public static final String COACH_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 코치 이름은 최소 2글자, 최대 4글자 사이여야 합니다.";
     public static final String INPUT_FORMAT_ERROR_MESSAGE = "[ERROR] 형식을 맞추어 다시 입력해주세요.";
+    public static final String CREW_SIZE_ERROR_MESSAGE = "[ERROR] 함께 식사하는 코치는 최소 2명, 최대 5명까지 가능합니다.";
+
 }
