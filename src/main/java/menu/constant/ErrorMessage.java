68ae5e360e2d7512416bd166cd3acbddfaa49b7e
seokmyungham <hellotjraud@gmail.com>
2023-12-15 14:28:08 +0900

    feat(ErrorMessage): INPUT_FORMAT_ERROR_MESSAGE 에러 메시지 추가

--- src/main/java/menu/constant/ErrorMessage.java
+++ src/main/java/menu/constant/ErrorMessage.java
@@ -2,5 +2,5 @@ package menu.constant;
 
 public class ErrorMessage {
     public static final String COACH_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 코치 이름은 최소 2글자, 최대 4글자 사이여야 합니다.";
-
+    public static final String INPUT_FORMAT_ERROR_MESSAGE = "[ERROR] 형식을 맞추어 다시 입력해주세요.";
 }
